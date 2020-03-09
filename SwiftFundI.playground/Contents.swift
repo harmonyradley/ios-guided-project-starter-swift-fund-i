2e0e597b4e8623b427cfb01cacf9cf07d2cad94f
Harmony Radley <harmonyradley99@gmail.com>
2020-03-09 13:40:52 -0400

    Arrays

--- SwiftFundI.playground/Contents.swift
+++ SwiftFundI.playground/Contents.swift
@@ -37,36 +37,79 @@ print("Lambda School is \(ls100) \(lambdaSchool)")
 //: ## Mutability
 //: There are two kinds of containers that hold values in Swift. One kind is called a constant, because once a value has been set, it cannot change for the lifetime of that container. Constants are declared with the `let` keyword.
 
+let students = 30
+let staff = 4
+let totatlPeople = students + staff
+print(totatlPeople)
 
+// totalPeople = 39
 
 //: Variables are declared with the 'var' keyword
+var appleCount = 10
+appleCount = appleCount + 5
+print(appleCount)
 
+// Compound Assignment
+appleCount -= 2
+print(appleCount)
 
+appleCount *= 2
+print(appleCount)
 
 //: ## Type Inference and Explicit Typing
 //: All containers in Swift must have a type set on them. The containers we've seen so far had their types set implicitly. This is called _type inference_. Meaning the kind of object stored in the container was determined by looking at what kind of value was assigned to the container.
 
-
+let ios = "Apple's mobile operating system"
 
 //: In the line above, the constant `ios` stores a value "Apple's mobile operating system". This value is a `String`, so the data type of `ios` is `String`. The type was _inferred_ by the value provided.
 //: Another way to type a container is to set it explicitly. See some examples of this below.
 
+let price: Double = 1299.99
+let onSale: Bool = false
+let modelName: String = "Macbook Pro"
+let discountQty: Int = 15
+
+// If statement will always assume true
+
+if onSale {
+    print("\(modelName) on sale for \(price)")
+} else {
+    print("\(modelName) at regular price: \(price). But at least \(discountQty) for special pricing")
+}
 
 
 //: ## Type Safety
 //: Once a container's data type is set, it cannot hold a value of any other type
+var life = 42
+
 
 
 
 //: ## Type Coercion
 //: Types are never automatically converted into another type.
+let label = "The width is "
+let width = 94
+let trueBool = true
 
+let widthLabel = label + String(width) + String(trueBool)
+print(widthLabel)
 
+let x: Int = 12
+let y: Double = 3.2
+
+Double(x) / y
 
 //: ## String Interpolation and Concatenation
 //: Using the `+` operator with two strings is called _string concatentation_. One string is appended to the end of another.
 
+// String Concatenation
+let heightLabel = "The height is" + " 100"
+print(heightLabel)
 
+// String Interpolation
+
+let anotherWidthLabel = "The width is \(width)"
+print(anotherWidthLabel)
 
 //: The below example shows the _string interpolation_ characters (`\()`). These allow a `String` (or non-string) value to be placed in-line inside a larger `String` literal value. If the value is not already a `String`, it will be converted.
 
@@ -76,15 +119,39 @@ print("Lambda School is \(ls100) \(lambdaSchool)")
 //: ### Arrays
 //: A Swift array is a collection of like objects, and they are stored in the order they are added. Accessing a particular element is done by using its index value (which starts at 0).
 
+let shipCaptains = ["Malcom Reynolds", "Jean-Luc Picard", "James T. Kirk", "Han Solo"]
+let sameCaptains = ["Malcom Reynolds", "Jean-Luc Picard", "James T. Kirk", "Han Solo"]
+let differentCaptains = ["Kathryn Janeway", "Poe Dameron"]
+
+// Arrays are Zero-Indexed - Start counting from Zero
+// Subscript Syntax arrayName[0]
+print(shipCaptains[2])
 
 
 //: Array equality is determined by looking at each element. If all elements are the same, and in the same order, the two arrays are equal.
 
-
+if shipCaptains == sameCaptains {
+    print("Arrays are equal")
+} else {
+    print("Arrays are not equal")
+}
 
 //: Since `shipCaptains` was declared as a constant, we must convert it to a mutable array before we can add entries.
 
+var moreCaptains = shipCaptains
+// There are 2 ways to insert elements into an array
+moreCaptains.append("Kathryn Janeway")
+print(moreCaptains)
 
+//moreCaptains[1] = "Poe Dameron" // Modifying a value in an array
+moreCaptains.insert("Poe Dameron", at: 1) // Adding a value at a given index
+print(moreCaptains)
+
+moreCaptains.remove(at: 2)
+
+print(moreCaptains)
+
+// Ordered collection of like elements 
 
 //: ### Dictionaries
 //: A Swift dictionary is similar to an array in that is a collection, but the items are stored in no particular order. To access an item, its associated key is provided. Dictionaries are also called "key-value" stores. The key has a type, and the value can have the same or a different type.
